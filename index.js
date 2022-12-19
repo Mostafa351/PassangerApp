69bb79fdcf7cfe6bec7328b72da6bf30f1874ee4
mostafa351 <eng.mostafa.salah.84@gmail.com>
2022-12-19 18:07:30 +0200

    Add counter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //document.getElementById("count-el").innerText = 5
+
+// initialise counter to 0
+let count = 0;
+
+// function increment(){
+//   count += 1;
+//   document.getElementById("count-el").innerText = count;
+// }
+
+document.getElementById("increment-btn").addEventListener('click',()=>{
+    count += 1;
+    document.getElementById("count-el").innerText = count;
+})
