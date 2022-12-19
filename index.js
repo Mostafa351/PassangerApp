18b4cd39b005c7bfd56ddd09ae1b9651eec27088
mostafa351 <eng.mostafa.salah.84@gmail.com>
2022-12-19 18:12:51 +0200

    add some comments

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 // initialise counter to 0
 let count = 0;
 
+// one way to listen to the click
 // function increment(){
 //   count += 1;
 //   document.getElementById("count-el").innerText = count;
 // }
 
+// another way to listen to the click
 document.getElementById("increment-btn").addEventListener('click',()=>{
+  // increment the counter
     count += 1;
+  // display the new count  
     document.getElementById("count-el").innerText = count;
 })
